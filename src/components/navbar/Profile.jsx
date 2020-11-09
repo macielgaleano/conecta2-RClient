f51bc6948469b961ec6cb56763d73fbb028c3f68
Maciel Galeano <macielgaleano.jh@gmail.com>
2020-11-09 13:30:40 -0300

    navar ended

--- src/components/navbar/Profile.jsx
+++ src/components/navbar/Profile.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import UseFetchUserData from "../../hooks/useFetchUserData";
+import { useSelector } from "react-redux";
 import "./navbar.css";
 import { Link } from "react-router-dom";
 
 export default function Profile() {
-  const profileData = UseFetchUserData("gallardo");
+  const profileData = UseFetchUserData(useSelector((state) => state.user.user[0].author));
   console.log(profileData);
 
   return (
