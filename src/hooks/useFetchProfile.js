2c40b49bcb265433a39a078bcd8b03f9c8bf75d1
Maciel Galeano <macielgaleano.jh@gmail.com>
2020-11-09 16:29:17 -0300

    updt

--- src/hooks/useFetchProfile.js
+++ src/hooks/useFetchProfile.js
@@ -1,23 +1,20 @@
 import { useState, useEffect } from "react";
-import {useParams} from "react-router-dom"
 
 import axios from "axios";
 
 const useFetchProfile = (username) => {
-  
   const [user, setUser] = useState("");
   const url = "https://conecta2-server.vercel.app/" + "users/" + username;
-    useEffect(() => {
-      axios
-        .get(url)
-        .then((res) => {
-            setUser(res.data);
-        })
-        .catch((err) => {
-          console.error(err);
-        });
-  
-  },[]);
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((res) => {
+        setUser(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
   return user;
 };
 
