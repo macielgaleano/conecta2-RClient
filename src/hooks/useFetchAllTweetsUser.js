277a8012af8f4b1895526edc90f22fa32efce491
Maciel Galeano <macielgaleano.jh@gmail.com>
2020-11-10 02:51:33 -0300

    Update useFetchAllTweetsUser.js

--- src/hooks/useFetchAllTweetsUser.js
+++ src/hooks/useFetchAllTweetsUser.js
@@ -5,7 +5,10 @@ export default function useFetchAllTweetsUser() {
   const user = useSelector((state) => state.user);
   const [tweets, setUserProfile] = useState(null);
   const url = "https://conecta2-server.vercel.app/" + "users/" + username;
-  const params = { Authorization: user.token };
+  const params = {
+    Authorization:
+      " eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmYTMwMTNjNzU3YmRiMmE1ZWUyODEwMyIsImlhdCI6MTYwNDkzMjQyOCwiZXhwIjoxNjA1MTQ4NDI4fQ.FKT2dTJcgm0osRYHSVVO_jt8rQpW2U5eQHSZUR9tKL8",
+  };
 
   useEffect(() => {
     if (!userProfile) {
